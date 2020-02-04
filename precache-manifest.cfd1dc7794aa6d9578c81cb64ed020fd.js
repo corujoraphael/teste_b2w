02ff5827857d1bce568b98ea0d58af5304266d82
raphael <corujoraphael@gmail.com>
2020-02-04 00:51:59 -0300

    Updates

---
+++ precache-manifest.cfd1dc7794aa6d9578c81cb64ed020fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55342123cb6cc7d57347bdae3f12f3b3",
+    "url": "/teste_b2w/index.html"
+  },
+  {
+    "revision": "a641e8403a3406b3ceea",
+    "url": "/teste_b2w/static/css/main.217e0b87.chunk.css"
+  },
+  {
+    "revision": "6a028d0689f2f32c88c4",
+    "url": "/teste_b2w/static/js/2.2a26da21.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/teste_b2w/static/js/2.2a26da21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a641e8403a3406b3ceea",
+    "url": "/teste_b2w/static/js/main.8b68402a.chunk.js"
+  },
+  {
+    "revision": "904f144ee5bb8d5e9fd9",
+    "url": "/teste_b2w/static/js/runtime-main.53e9ab55.js"
+  }
+]);
